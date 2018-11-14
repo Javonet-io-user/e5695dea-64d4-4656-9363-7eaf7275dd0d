0e89ea1125ba21bb07daae60753aa2e03cbaa8d4
Javonet <support@javonet.com>
2018-11-14 11:47:22 +0000

    init

---
+++ src/jio/System/Windows/Forms/Control.java
@@ -0,0 +1,30 @@
+package jio.System.Windows.Forms;import Common.Activation;import static Common.Helper.Convert;import static Common.Helper.getGetObjectName;import static Common.Helper.getReturnObjectName;import static Common.Helper.ConvertToConcreteInterfaceImplementation;import Common.Helper;import com.javonet.Javonet;
+                        import com.javonet.JavonetException;
+                        import com.javonet.JavonetFramework;
+                        import com.javonet.api.NObject;
+                        import com.javonet.api.NEnum;
+                        import com.javonet.api.keywords.NRef;
+                        import com.javonet.api.keywords.NOut;
+                        import com.javonet.api.NControlContainer;import java.util.concurrent.atomic.AtomicReference;import jio.System.ComponentModel.*;
+import jio.System.Windows.Forms.*;
+import jio.System.*;public class Control extends Component implements IComponent,IDropTarget,ISynchronizeInvoke,IWin32Window,IBindableComponent {protected NObject javonetHandle; public Control (){  super((NObject) null); try {  javonetHandle = Javonet.New("Control"); super.setJavonetHandle(javonetHandle); } catch (JavonetException _javonetException) { _javonetException.printStackTrace(); } }public Control (java.lang.String text){  super((NObject) null); try {  javonetHandle = Javonet.New("Control",text); super.setJavonetHandle(javonetHandle); } catch (JavonetException _javonetException) { _javonetException.printStackTrace(); } }public Control (java.lang.String text,java.lang.Integer left,java.lang.Integer top,java.lang.Integer width,java.lang.Integer height){  super((NObject) null); try {  javonetHandle = Javonet.New("Control",text,left,top,width,height); super.setJavonetHandle(javonetHandle); } catch (JavonetException _javonetException) { _javonetException.printStackTrace(); } }public Control (Control parent,java.lang.String text){  super((NObject) null); try {  javonetHandle = Javonet.New("Control",parent,text); super.setJavonetHandle(javonetHandle); } catch (JavonetException _javonetException) { _javonetException.printStackTrace(); } }public Control (Control parent,java.lang.String text,java.lang.Integer left,java.lang.Integer top,java.lang.Integer width,java.lang.Integer height){  super((NObject) null); try {  javonetHandle = Javonet.New("Control",parent,text,left,top,width,height); super.setJavonetHandle(javonetHandle); } catch (JavonetException _javonetException) { _javonetException.printStackTrace(); } }public Control(NObject handle) {super(handle);this.javonetHandle=handle;}public void setJavonetHandle(NObject handle) {
+                    this.javonetHandle = handle;
+                }/**
+	 * Method
+	 */
+            public void OnDragEnter (DragEventArgs drgEvent){ try { javonetHandle.invoke("OnDragEnter",drgEvent);} catch (JavonetException _javonetException) { _javonetException.printStackTrace(); } }/**
+	 * Method
+	 */
+            public void OnDragLeave (EventArgs e){ try { javonetHandle.invoke("OnDragLeave",e);} catch (JavonetException _javonetException) { _javonetException.printStackTrace(); } }/**
+	 * Method
+	 */
+            public void OnDragDrop (DragEventArgs drgEvent){ try { javonetHandle.invoke("OnDragDrop",drgEvent);} catch (JavonetException _javonetException) { _javonetException.printStackTrace(); } }/**
+	 * Method
+	 */
+            public void OnDragOver (DragEventArgs drgEvent){ try { javonetHandle.invoke("OnDragOver",drgEvent);} catch (JavonetException _javonetException) { _javonetException.printStackTrace(); } }	static {
+		try {
+			Activation.initializeJavonet();
+		} catch (java.lang.Exception e) {
+			e.printStackTrace();
+		}
+	}}
